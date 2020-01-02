a804b4c2e2f5d7c32562aef217e37a3bebb183c4
Felix Geelhaar <felix.geelhaar@sixt.com>
2020-01-02 21:25:29 +0100

    feature: include a Gatsby Theme for blogs and start with customization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,36 @@
 module.exports = {
   siteMetadata: {
-    title: `Felix Geelhaars Blog`,
-    description: `A simple blog of Felix Geelhaar`,
-    author: `Felix Geelhaar`,
+    title: "My Blog",
+    author: "Felix Geelhaar",
+    description: "A collection of my thoughts and writings.",
+    siteUrl: "https://felixgeelhaar.blog",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/felixgeelhaar",
+      },
+      {
+        name: "github",
+        url: "https://github.com/felixgeelhaar",
+      },
+    ],
   },
   plugins: [
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `posts`,
-      },
-    },
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+      resolve: "gatsby-theme-blog",
+      options: {},
     },
     `gatsby-plugin-emotion`,
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `Felix Geelhaar`,
+        short_name: `Felix Geelhaar`,
         start_url: `/`,
         background_color: `#6b37bf`,
         theme_color: `#6b37bf`,
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `content/assets/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default () => (
+  <>
+    This is my updated bio.
+    <br />
+    It's shadowing the content from the theme.
+  </>
+)
