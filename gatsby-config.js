1c0ec0c388f316f103cff2babd41af05c4e5660b
Felix Geelhaar <felix.geelhaar@sixt.com>
2020-01-02 14:48:41 +0100

    Initial commit with own configurations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,46 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Felix Geelhaars Blog`,
+    description: `A simple blog of Felix Geelhaar`,
+    author: `Felix Geelhaar`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `posts`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+  ],
 }
---
+++ posts/first-blog-post/first-blog-post.md
@@ -0,0 +1,8 @@
+---
+title: First Blog Post
+slug: first-post
+author: Felix Geelhaar
+date: 2020-01-01
+---
+
+This is the first blog post
